2da56f60dde26d7dbd1f2460ac7639a22231044b
Pavithra Prathiraja <148129933+Pavi-NP@users.noreply.github.com>
2024-07-03 22:54:24 +1000

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="content">
+            <h2>Welcome to the GenAI App</h2>
+            <p>This is the overview of the web application.</p>
+        </div>
+    );
+}
+
+export default Home;
