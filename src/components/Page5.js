0045f54bd281885bf991d5611174af35b21e6eb5
Pavithra Prathiraja <148129933+Pavi-NP@users.noreply.github.com>
2024-07-03 22:57:04 +1000

    Create Page5.js

---
+++ src/components/Page5.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page5() {
+    return (
+        <div className="content">
+            <h2>Page 5</h2>
+            <p>Content for Page 5</p>
+        </div>
+    );
+}
+
+export default Page5;
