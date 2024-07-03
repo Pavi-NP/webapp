e1363aa6d62498c9305ec7485ca14d541e3a02fc
Pavithra Prathiraja <148129933+Pavi-NP@users.noreply.github.com>
2024-07-03 22:56:41 +1000

    Create Page4.js

---
+++ src/components/Page4.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page4() {
+    return (
+        <div className="content">
+            <h2>Page 4</h2>
+            <p>Content for Page 4</p>
+        </div>
+    );
+}
+
+export default Page4;
