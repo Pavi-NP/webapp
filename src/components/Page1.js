4073a3cbb5ef8162c3e8cf2cf1e00fe6bca61b82
Pavithra Prathiraja <148129933+Pavi-NP@users.noreply.github.com>
2024-07-03 22:55:00 +1000

    Create Page1.js

---
+++ src/components/Page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page1() {
+    return (
+        <div className="content">
+            <h2>Page 1</h2>
+            <p>Content for Page 1</p>
+        </div>
+    );
+}
+
+export default Page1;
