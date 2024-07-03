609cad31b570e45d3b301a1cbf25ef71f48d5048
Pavithra Prathiraja <148129933+Pavi-NP@users.noreply.github.com>
2024-07-03 22:55:28 +1000

    Create Page2.js

---
+++ src/components/Page2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page2() {
+    return (
+        <div className="content">
+            <h2>Page 2</h2>
+            <p>Content for Page 2</p>
+        </div>
+    );
+}
+
+export default Page2;
